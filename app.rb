40893a5451791ed13b3a0033bbf82201d6913f83
Gabrijela Gavric and Qudsia Ahmad <qudsia@>
2017-09-19 10:06:21 -0700

    add views and integration files

--- app.rb
+++ app.rb
@@ -3,3 +3,17 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/task')
 require('pry')
+
+get("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list)
+end
+
+post("/tasks") do
+  description = params.fetch("description")
+  list_id = params.fetch("list_id").to_i()
+  @list = List.find(list_id)
+  @task = Task.new({:description => description, :list_id => list_id})
+  @task.save()
+  erb(:task_success)
+end
